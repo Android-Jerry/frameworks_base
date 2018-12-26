8e2c18f9a4f6356c1994f863c0b0a8ba3f328a9d
Jerry <isjerryxiao@outlook.com>
2018-12-26 21:03:05 +0800

    delete unused settings

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4438,18 +4438,6 @@ public final class Settings {
          */
 
         /**
-         *  Enable statusbar double tap gesture on to put device to sleep
-         * @hide
-         */
-        public static final String DOUBLE_TAP_SLEEP_GESTURE = "double_tap_sleep_gesture";
-
-        /**
-         * Enable double tap gesture anywhere on the lock screen put device to sleep
-         * @hide
-         */
-        public static final String DOUBLE_TAP_SLEEP_ANYWHERE = "double_tap_sleep_anywhere";
-
-        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
